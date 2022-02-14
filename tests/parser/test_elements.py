ade25bd6b8ed39dc1985d79ca2fd9417a73f3afa
William Lee <wlee753159@gmail.com>
2022-02-14 01:13:44 +0000

    Refactor element tests

--- tests/parser/test_elements.py
+++ tests/parser/test_elements.py
@@ -8,9 +8,13 @@ Tests the parsing of element symbols
 
 import pytest
 
-from pydepict.consts import ELEMENTS
+from pydepict.consts import CLOSE_BRACKET, ELEMENTS, OPEN_BRACKET
 from pydepict.errors import ParserError
-from pydepict.parser import Stream, parse_element_symbol
+from pydepict.parser import Stream, parse, parse_atom, parse_element_symbol
+
+BRACKET_ATOM_TEMPLATE = f"{OPEN_BRACKET}{{}}{CLOSE_BRACKET}"
+
+NONEXISTENT_SYMBOLS = "Fg Ak Of My Dj".split()
 
 
 @pytest.mark.parametrize("element", ELEMENTS)
@@ -22,11 +26,21 @@ def test_parse_valid_symbols(element: str):
     result = parse_element_symbol(stream)
     assert result == element
 
+    stream = Stream(BRACKET_ATOM_TEMPLATE.format(element))
+    result = parse_atom(stream)
+    assert result["element"] == element
+
+    stream = Stream(BRACKET_ATOM_TEMPLATE.format(element))
+    result = parse(stream)
+    assert result.nodes[0]["element"] == element
+
 
 @pytest.mark.parametrize(
-    "element", [element.lower() for element in ELEMENTS if element.isalpha()]
+    "element",
+    [element.lower() for element in ELEMENTS if element.isalpha()]  # Lowercase symbols
+    + NONEXISTENT_SYMBOLS,
 )
-def test_parse_lowercase_symbols(element: str):
+def test_parse_invalid_symbols(element: str):
     """
     Tests parsing lowercase element symbols, which should return :data:`None`
     """
@@ -34,12 +48,10 @@ def test_parse_lowercase_symbols(element: str):
     with pytest.raises(ParserError):
         parse_element_symbol(stream)
 
-
-@pytest.mark.parametrize("element", ("Fg", "Ak", "Of", "My", "Dj"))
-def test_parse_nonexistent_symbols(element):
-    """
-    Tests parsing nonexistent element symbols, which should raise an error
-    """
-    stream = Stream(element)
+    stream = Stream(BRACKET_ATOM_TEMPLATE.format(element))
     with pytest.raises(ParserError):
-        parse_element_symbol(stream)
+        parse_atom(stream)
+
+    stream = Stream(BRACKET_ATOM_TEMPLATE.format(element))
+    with pytest.raises(ParserError):
+        parse(stream)
