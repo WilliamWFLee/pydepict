5b42410a5103da37cff982ea40189a76079989c4
William Lee <wlee753159@gmail.com>
2022-02-14 15:23:33 +0000

    Docstring errors.py

--- pydepict/errors.py
+++ pydepict/errors.py
@@ -8,10 +8,32 @@ Custom error classes
 
 
 class ParserError(Exception):
+    """
+    Error class for all parser errors
+    """
     def __init__(self, msg: str, position: int) -> None:
+        """
+        Initialises an instance of a parser error
+
+        :param msg: The error message
+        :type msg: str
+        :param position: The position within the stream at which the error occurred
+        :type position: int
+        """
         super().__init__(f"{msg}, pos {position}")
 
 
 class ParserWarning(Warning):
+    """
+    Warning class for all parser warnings
+    """
     def __init__(self, msg: str, position: int) -> None:
+        """
+        Initialises an instance of a parser warning
+
+        :param msg: The warning message
+        :type msg: str
+        :param position: The position within the stream at which the error occurred
+        :type position: int
+        """
         super().__init__(f"{msg}, pos {position}")
