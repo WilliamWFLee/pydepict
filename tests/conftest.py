26dd9a4eeb9e6b10230672d8a490df37dfb113a5
William Lee <wlee753159@gmail.com>
2022-02-28 01:14:12 +0000

    Make must_pass pytest mark actually work

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,13 +3,23 @@
 import pytest
 
 
+def get_root_ancestor(item: pytest.Item) -> pytest.Item:
+    parent = item.parent
+    while True:
+        if parent.parent is None:
+            break
+        parent = parent.parent
+
+    return parent
+
+
 def pytest_runtest_makereport(item: pytest.Item, call: pytest.CallInfo[None]):
-    if item.iter_markers(name="must_pass"):
+    if item.get_closest_marker(name="must_pass"):
         if call.excinfo is not None:
-            item.parent._mpfailed = item
+            get_root_ancestor(item)._mpfailed = item
 
 
 def pytest_runtest_setup(item: pytest.Item):
-    failed_must_pass: pytest.Item = getattr(item.parent, "_mpfailed", None)
-    if failed_must_pass is not None:
-        pytest.skip(f"{failed_must_pass.name} is marked as must passed.")
+    ancestor = get_root_ancestor(item)
+    if getattr(ancestor, "_mpfailed", None) is not None:
+        pytest.skip(f"{ancestor._mpfailed.name} is marked as must passed.")
